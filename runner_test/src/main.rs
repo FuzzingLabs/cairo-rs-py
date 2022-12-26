1a4057168fc69aa0109978ee48a74412dd9ac069
Nabih <nabih@fuzzinglabs.com>
2022-12-26 14:54:55 +0100

    Add rust runner

--- runner_test/src/main.rs
+++ runner_test/src/main.rs
@@ -1,49 +1,13 @@
-
-use std::env;
-use std::fs;
-mod parse_json;
-mod cairo_rs_py;
 use cairo_rs_py::cairo_runner::PyCairoRunner;
-/* use cairo_rs_py;
-use cairo_rs_py::cairo_runner as cairo_rs_py_runner;
-use cairo_rs_py_runner::PyCairoRunner; */
-use crate::parse_json::parse_json;
-mod utils;
-
-fn py_runner(json: &String, func_name: String, args_num: u64, data: isize) {
-    println!("Running py_runner");
-    let mut cairo_runner = PyCairoRunner::new(json.to_string(), Some(func_name.clone()), Some("all".to_string()), false).unwrap();
-    //cairo_runner.initialize_function_runner();
-    cairo_runner.initialize_segments();
-/*     Python::with_gil(|py| {
-        cairo_runner
-            .run_from_entrypoint(
-                py,
-                py.eval("0", None, None).unwrap(),
-                Vec::<&PyAny>::new().to_object(py),
-                None,
-                None,
-                Some(false),
-                None,
-                None,
-            )
-            .unwrap();
-    }); */
-    let entrypoint: &str = &func_name.clone();
-    cairo_runner.cairo_run_py(true, None, None, None, None, Some(entrypoint)).expect("could not run program");
-}
-
+use std::fs;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        println!("Usage : cargo run -- <PATH>");
-        return;
-    }
-    let filename: &String = &args[1];
-    let functions = parse_json(filename);
-    let contents = fs::read_to_string(filename).expect("could not read contract artefact");
-    for function in functions {
-        py_runner(&contents, function.name, function.num_args, 5);
-    }
+    let path = "../cairo_programs/assert_not_zero.json".to_string();
+    let program = fs::read_to_string(path).unwrap();
+    let mut runner =
+        PyCairoRunner::new(program, Some("main".to_string()), None, false).unwrap();
+    runner
+        .cairo_run_py(false, None, None, None, None, None)
+        .expect("Couldn't run program");
+
 }
